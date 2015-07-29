fb965ae4d23b4596abbb5ae67b1b7143e2fc498a
sakura <sakura@ubuntu.ubuntu-domain>
2015-07-29 23:58:58 +0800

    test

--- web/front.php
+++ web/front.php
@@ -9,8 +9,20 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
 
 $request = Request::createFromGlobals();
+$routes = new RouteCollection();
+$routes->add('hello',new Route('/hello/{name}',array('name','world')));
+$routes->add('bye',new Route('/bye'));
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes,$context);
+$attributes = $matcher->match($request->getPathInfo());
+
 $response = new Response();
 
 $map = array(
