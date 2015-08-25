5e323285c7988a6550081f0cf97732b02a46dc8d
sakurallj <liaolingjia@163.com>
2015-08-26 00:41:46 +0800

    front controller

--- web/front.php
+++ web/front.php
@@ -9,36 +9,23 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 
 $request = Request::createFromGlobals();
-$routes = new RouteCollection();
-$routes->add('hello',new Route('/hello/{name}',array('name','world')));
-$routes->add('bye',new Route('/bye'));
-$context = new RequestContext();
-$context->fromRequest($request);
-$matcher = new UrlMatcher($routes,$context);
-$attributes = $matcher->match($request->getPathInfo());
-
 $response = new Response();
 
 $map = array(
-    '/hello'=> __DIR__.'/../src/hello.php',
-    '/bye'=> __DIR__.'/../src/bye.php'
+    '/hello' => __DIR__.'/../src/pages/hello.php',
+    '/bye'   => __DIR__.'/../src/pages/bye.php',
 );
 
 $path = $request->getPathInfo();
-if(isset($map[$path])){
+if (isset($map[$path])) {
     ob_start();
     include $map[$path];
     $response->setContent(ob_get_clean());
-}
-else{
+} else {
     $response->setStatusCode(404);
-    $response->setContent('page not fund');
+    $response->setContent('Not Found');
 }
 
-$response->send('www');
+$response->send();
